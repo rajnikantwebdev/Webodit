fadc0e27117eb0d04cc5e3e2fa8d764a0d7f6ae6
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-12-18 00:16:40 +0530

    Changes

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FormSection from '../sections/FormSection'
+import ProjectSection from '../sections/ProjectSection'
 
 function HomePage() {
   return (
-    <div>
+    <div className='w-full overflow-hidden'>
         <Hero/>
         <FormSection/>
-      
+        <ProjectSection/>
     </div>
   )
 }
--- src/sections/FormSection.jsx
+++ src/sections/FormSection.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { AOS } from 'aos'
+import 'aos/dist/aos.css'
 
 function FormSection() {
+
+
   return (
     <div>
-<div class="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
+
+  <div data-aos="fade-left">    
+<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
+<p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+<a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+    Learn more
+    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+  </svg>
+</a>
+</div>
+<div class="min-h-screen p-6  flex items-center justify-center">
   <div class="container w-[100vw] mx-auto">
-    <div>
+    <div data-aos="fade-left">
       <h2 class="font-semibold text-xl text-gray-600">Responsive Form</h2>
       <p class="text-gray-500 mb-6">Form is mobile responsive. Give it a try.</p>
 

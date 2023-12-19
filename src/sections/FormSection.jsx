a783c42aa3830bd6af4ec018084fc48da1c2cfea
Rajnikantbhay <rsusishere@gmail.com>
2023-12-19 21:08:44 +0530

    changes

--- src/sections/FormSection.jsx
+++ src/sections/FormSection.jsx
@@ -7,7 +7,7 @@ function FormSection() {
 
 
   return (
-    <div className='bg-blue-200 w-full py-4' >
+    <div className='bg-blue-200 w-full' >
 
   <div data-aos="fade-left">    
 <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>

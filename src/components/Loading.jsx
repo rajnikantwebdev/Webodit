ef1eded54ddf9ee92f04fe147d18cd95267e35e8
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-12-21 12:11:52 +0530

    Push Kar na

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import spaceShip from '../assets/spaceship.json'
+import Lottie from 'react-lottie';
+import loadingAnimation from '../assets/loadingAnimation.json'
 
 function Loading() {
+
+
+  const animationOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loadingAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+
+
   return (
-    <div className='text-xl font-bold w-[100vw] flex justify-center m-auto align-center'>
-      <spaceShip/>
+    <div className='h-screen text-white text-xl font-bold w-[100vw] flex flex-col justify-center m-auto align-center'>
+
+<div className='ml-auto mr-auto align-self-center' style={{ width: '600px', height: '600px' }}>
+      <Lottie options={animationOptions} />
+</div>
+  
     </div>
   )
 }

2e91ee405d8313948548e8630abbc36f63395415
Rajnikantbhay <rsusishere@gmail.com>
2023-12-21 16:09:28 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
 import Navbar from './components/Navbar'
 import './App.css'
 import HomePage from './Pages/HomePage'
-import Loading from './components/Loading'
+// import Loading from './components/Loading'
 import { useEffect,useState } from 'react'
 
 
 function App() {
 
   const [loadingState, setLoadingState] = useState(true)
-  useEffect(() => {
-    const fakeDataFetch = () => {
-      setTimeout(() => {
+  // useEffect(() => {
+  //   const fakeDataFetch = () => {
+  //     setTimeout(() => {
 
-        setLoadingState(false)
+  //       setLoadingState(false)
 
-      }, 4000)
+  //     }, 4000)
 
-    }
+  //   }
 
-    fakeDataFetch()
-  }, [])
+  //   fakeDataFetch()
+  // }, [])
   return (
-
-
-    loadingState ? (<Loading />) :
-      (
         <div className='w-[99vw] overflow-x-hidden'>
           <Navbar />
           <HomePage />
         </div>
-      )
   )
 }
 

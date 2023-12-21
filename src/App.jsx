8f75d9ce20db5cb325e6778eae83b95a241364a5
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-12-21 10:30:50 +0530

    Great Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import Navbar from './components/Navbar'
 import './App.css'
 import HomePage from './Pages/HomePage'
+import Loading from './components/Loading'
+import { useEffect,useState } from 'react'
+
 
 function App() {
+
+  const [loadingState, setLoadingState] = useState(true)
+  useEffect(() => {
+    const fakeDataFetch = () => {
+      setTimeout(() => {
+
+        setLoadingState(false)
+
+      }, 4000)
+
+    }
+
+    fakeDataFetch()
+  }, [])
   return (
-    <div className= 'w-[99vw] overflow-x-hidden'>
-      <Navbar />
-      <HomePage/>
-    </div>
+
+
+    loadingState ? (<Loading />) :
+      (
+        <div className='w-[99vw] overflow-x-hidden'>
+          <Navbar />
+          <HomePage />
+        </div>
+      )
   )
 }
 
---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import spaceShip from '../assets/spaceship.json'
+
+function Loading() {
+  return (
+    <div className='text-xl font-bold w-[100vw] flex justify-center m-auto align-center'>
+      <spaceShip/>
+    </div>
+  )
+}
+
+export default Loading
